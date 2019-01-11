eedbd814ff1559d5c7ca747b35669a456b383d4d
jihunhong <redgee49@gmail.com>
2019-01-12 05:16:43 +0900

    Default Og Preview Template Commit.

--- src/main/java/me/vscode/ogpreviewer/Controller/HomeController.java
+++ src/main/java/me/vscode/ogpreviewer/Controller/HomeController.java
@@ -12,20 +12,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController{
 
-    @RequestMapping("/search")
+    @RequestMapping("/")
     public String home(Model model){
+        
+        return "home";
+    }
+
+    @RequestMapping("/search")
+    public String Search(Model model){
         String description = "";
         String image_url   = "";
+        String title = "";
+        String url = "https://www.daum.net";
         try{
-			String url = "https://www.daum.net";
+			
 
 			Document document = Jsoup.connect(url).get();
 
             Element meta_description = document.select("meta[property=og:description]").first();
             Element meta_image_url = document.select("meta[property=og:image]").first();
+            Element meta_title = document.select("meta[property=og:title]").first();
             
             description = meta_description.attr("content");
             image_url   = meta_image_url.attr("content");
+            title       = meta_title.attr("content");
 
 		}catch(IOException e){
 			e.printStackTrace();
@@ -33,6 +43,8 @@ public class HomeController{
         
         model.addAttribute("description",description);
         model.addAttribute("image_url",image_url);
+        model.addAttribute("title",title);
+        model.addAttribute("url",url);
 
         return "home";
     }
