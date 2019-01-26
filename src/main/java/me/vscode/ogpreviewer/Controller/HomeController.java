29e3844e072c4f8cd1576633ff5d89abdbe143d3
jihunhong <redgee49@gmail.com>
2019-01-26 19:54:52 +0900

    config server.port

--- src/main/java/me/vscode/ogpreviewer/Controller/HomeController.java
+++ src/main/java/me/vscode/ogpreviewer/Controller/HomeController.java
@@ -1,35 +1,43 @@
 package me.vscode.ogpreviewer.Controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController{
+public class HomeController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
-    public String home(Model model){
-        
+    public String home(Model model) {
+
         return "home";
     }
 
     @RequestMapping("/search")
-    public String Search(Model model, HttpServletRequest request){
+    public String Search(Model model, HttpServletRequest request) {
         String description = "";
-        String image_url   = "";
+        String image_url = "";
         String title = "";
-        String url = request.getParameter("query");
-        try{
-			
 
-			Document document = Jsoup.connect(url).get();
+        String url = request.getParameter("query");
+
+        try {
+
+            Document document = Jsoup.connect(url).get();
 
             Element meta_description = document.select("meta[property=og:description]").first();
             Element meta_image_url = document.select("meta[property=og:image]").first();
@@ -49,6 +57,48 @@ public class HomeController{
         model.addAttribute("url",url);
 
         return "search_result";
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+
+        return "test";
+    }
+
+    @RequestMapping("/test_result")
+    public String Test(Model model, HttpServletRequest request) {
+
+        String url = request.getParameter("query");
+        Map<String, String> map = new HashMap<>();
+
+        String title = "";
+        String a = "";
+
+        try{
+            
+            Document document = Jsoup.connect(url).get();
+            
+            Elements els = document.getElementsByClass("usertxt");
+            Element ac = document.select("nickname in").first();
+
+            a = ac.attr("title");
+            
+            title = els.attr("title");
+            
+            for(Element el : els){
+                    map.put(el.data(), el.toString());
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        model.addAttribute("map", map);
+        model.addAttribute("title", title);
+        model.addAttribute("a", a);
+
+        return "test_page";
+        
     }
 
 }
--- src/test/java/me/vscode/ogpreviewer/DemoApplicationTests.java
+++ src/test/java/me/vscode/ogpreviewer/DemoApplicationTests.java
@@ -1,18 +1,26 @@
 package me.vscode.ogpreviewer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Test
 	public void contextLoads() { 
@@ -29,6 +37,43 @@ public class DemoApplicationTests {
 
             System.out.println(element.toString());
  
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void Pairing_tags(){
+		try{
+			String url = "https://www.daum.net";
+
+			Document document = Jsoup.connect(url).get();
+			
+            Elements els = document.select("meta");
+
+            Map<String, String> map = new HashMap<>();
+			
+			for(Element el : els){
+                map.put(el.attr("property"), el.attr("content"));
+                System.out.println(el.attr("property") + "  :  " + el.attr("content"));                
+            }
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void Crawl(){
+		try{
+			String url = "http://gall.dcinside.com/mgallery/board/view/?id=mnet_k&no=3103286&exception_mode=recommend&page=1";
+
+			Document document = Jsoup.connect(url).get();
+			
+			Elements els = document.select(".usertxt ub-word");
+			
+			for(Element el : els){
+                String s = el.text();               
+            }
 		}catch(IOException e){
 			e.printStackTrace();
 		}
